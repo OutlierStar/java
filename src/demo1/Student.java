54950fe6d19ecdb17bb6e9fe9397911bc72b72dc
Outlier <86137696+OutlierStar@users.noreply.github.com>
2023-04-13 00:46:46 +0000

    4.13

---
+++ src/demo1/Student.java
@@ -0,0 +1,20 @@
+package demo1;
+
+public class Student {
+    private int id;
+    private String name;
+    private String YuWenScore;
+
+    public void setId(String string) {
+    }
+
+    public void setName(String string) {
+    }
+
+    public void setYuWenScore(float parseFloat) {
+    }
+
+    public void setShuXueScore(float parseFloat) {
+    }
+
+}
